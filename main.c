30e70c8bcffa3369baf97385712c4987c5b6feda
alexisGalarzaSustaita <a355316@alumnos.uaslp.mx>
2024-11-28 18:24:07 -0600

    Create main.c

---
+++ main.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+int main(int argc, char** argv) {
+    //char name[50];
+
+    //printf("Enter a Data Dictionary Name: ");
+    
+    //fgets(name, sizeof(name), stdin);
+
+    mainMenu();
+
+    //FILE* dataDictionary = initializeDataDictionary(name); 
+    
+    //createEntity(dataDictionary);
+    //createEntity(dataDictionary);
+    //createEntity(dataDictionary); 
+
+    //captureEntities(dataDictionary);
+   // captureAttributesForEntity(dataDictionary);
+
+    //showEntitiesWithAttributes(dataDictionary);
+
+    //fclose(dataDictionary); 
+
+    return 0; 
+}
+
+//checar print attribute List
+//delete entity
+//delete attribute
+//modify attribute 
+//modify entity
+//No abre el diccionario ya creado, lo sobreescribe
